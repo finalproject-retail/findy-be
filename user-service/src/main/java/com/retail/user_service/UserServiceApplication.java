ae8f3c2771bd7c0647759cad195e46848e241b83
seoyoomi <seoyoomi1102@gmail.com>
2026-05-08 14:40:58 +0900

    style: 타입명 오타 수정

--- user-service/src/main/java/com/retail/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/retail/user_service/UserServiceApplication.java
@@ -9,8 +9,8 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-    	dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
