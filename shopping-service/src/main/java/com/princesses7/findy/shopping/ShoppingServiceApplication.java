30b3662a122049bed28ea2345a6312eddea37e95
gyuoo <yeomky98@naver.com>
2026-05-08 17:11:54 +0900

    chore: shopping-service 프로젝트 초기 구성 추가

---
+++ shopping-service/src/main/java/com/princesses7/findy/shopping/ShoppingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.princesses7.findy.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingServiceApplication.class, args);
+	}
+
+}
