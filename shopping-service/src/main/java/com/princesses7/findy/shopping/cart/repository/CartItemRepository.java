df45cec436c9dd5921801dddc187cebd8ed8dc43
gyuoo <yeomky98@naver.com>
2026-05-20 16:44:22 +0900

    feat: 사용자 상품 기준 장바구니 조회 메서드 추가

--- shopping-service/src/main/java/com/princesses7/findy/shopping/cart/repository/CartItemRepository.java
+++ shopping-service/src/main/java/com/princesses7/findy/shopping/cart/repository/CartItemRepository.java
@@ -1,9 +1,16 @@
 package com.princesses7.findy.shopping.cart.repository;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.princesses7.findy.shopping.cart.entity.CartItem;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
+	List<CartItem> findAllByCartUserIdAndProductIdIn(
+		Long userId,
+		Collection<Long> productIds
+	);
 }
