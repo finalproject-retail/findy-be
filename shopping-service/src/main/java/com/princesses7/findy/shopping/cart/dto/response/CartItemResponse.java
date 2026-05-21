f7a4b67d9b5a24dacdef55704bc2d809466ed32a
gyuoo <yeomky98@naver.com>
2026-05-21 10:24:10 +0900

    feat: 장바구니 상품 상세 응답 추가

--- shopping-service/src/main/java/com/princesses7/findy/shopping/cart/dto/response/CartItemResponse.java
+++ shopping-service/src/main/java/com/princesses7/findy/shopping/cart/dto/response/CartItemResponse.java
@@ -1,20 +1,28 @@
 package com.princesses7.findy.shopping.cart.dto.response;
 
 import com.princesses7.findy.shopping.cart.entity.CartItem;
+import com.princesses7.findy.shopping.product.dto.response.ProductSummaryResponse;
 
 public record CartItemResponse(
 	Long cartItemId,
 	Long productId,
+	ProductSummaryResponse product,
 	int quantity,
-	boolean checked
+	boolean checked,
+	int itemTotalAmount
 ) {
 
-	public static CartItemResponse from(CartItem cartItem) {
+	public static CartItemResponse from(
+		CartItem cartItem,
+		ProductSummaryResponse product
+	) {
 		return new CartItemResponse(
 			cartItem.getCartItemId(),
 			cartItem.getProductId(),
+			product,
 			cartItem.getQuantity(),
-			cartItem.isChecked()
+			cartItem.isChecked(),
+			product.calculateAmount(cartItem.getQuantity())
 		);
 	}
 }
