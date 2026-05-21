52293c79af647303821afb59f0b8a1b2f7f208d4
gyuoo <yeomky98@naver.com>
2026-05-21 17:09:41 +0900

    chore: recommendation-service 초기 환경 설정

--- recommendation-service/src/main/java/com/princesses7/findy/recommendation/RecommendationServiceApplication.java
+++ recommendation-service/src/main/java/com/princesses7/findy/recommendation/RecommendationServiceApplication.java
@@ -2,12 +2,23 @@ package com.princesses7.findy.recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class RecommendationServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+			.ignoreIfMissing()
+			.load();
+
+		dotenv.entries().forEach(entry ->
+			System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(RecommendationServiceApplication.class, args);
 	}
-
 }
