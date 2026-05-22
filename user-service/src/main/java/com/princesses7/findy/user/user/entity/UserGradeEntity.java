ef17db9eb666a0b758712870fa30b89e8c53ce0e
gyuoo <yeomky98@naver.com>
2026-05-22 14:15:11 +0900

    fix: 회원 등급 기본키 자동 증가 설정 추가

--- user-service/src/main/java/com/princesses7/findy/user/user/entity/UserGradeEntity.java
+++ user-service/src/main/java/com/princesses7/findy/user/user/entity/UserGradeEntity.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -22,6 +24,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class UserGradeEntity extends BaseTimeEntity {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "grade_id")
 	private Long gradeId;
 
