800b426cc8f565b63c156b12378da1d51eaeb5f9
gyuoo <yeomky98@naver.com>
2026-05-26 18:59:01 +0900

    feat: 프로모션 관리자 조회 repository 메서드 추가

--- shopping-service/src/main/java/com/princesses7/findy/shopping/promotion/repository/PromotionRepository.java
+++ shopping-service/src/main/java/com/princesses7/findy/shopping/promotion/repository/PromotionRepository.java
@@ -2,11 +2,30 @@ package com.princesses7.findy.shopping.promotion.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.princesses7.findy.shopping.promotion.entity.Promotion;
+import com.princesses7.findy.shopping.promotion.entity.PromotionStatus;
 
 public interface PromotionRepository extends JpaRepository<Promotion, Long> {
 
 	Optional<Promotion> findByPromotionId(Long promotionId);
+
+	Page<Promotion> findByPromotionNameContainingIgnoreCase(
+		String keyword,
+		Pageable pageable
+	);
+
+	Page<Promotion> findByStatus(
+		PromotionStatus status,
+		Pageable pageable
+	);
+
+	Page<Promotion> findByPromotionNameContainingIgnoreCaseAndStatus(
+		String keyword,
+		PromotionStatus status,
+		Pageable pageable
+	);
 }
