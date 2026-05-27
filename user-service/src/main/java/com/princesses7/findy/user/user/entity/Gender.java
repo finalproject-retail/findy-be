59e77f4dcad17f33abb766e0bde1b94fccee7b3f
seoyoomi <seoyoomi1102@gmail.com>
2026-05-27 18:21:57 +0900

    fix: user-service Enum 대소문자 무관 JSON 역직렬화 처리

--- user-service/src/main/java/com/princesses7/findy/user/user/entity/Gender.java
+++ user-service/src/main/java/com/princesses7/findy/user/user/entity/Gender.java
@@ -1,5 +1,12 @@
 package com.princesses7.findy.user.user.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Gender {
-	MALE, FEMALE
+	MALE, FEMALE;
+
+	@JsonCreator
+	public static Gender from(String value) {
+		return Gender.valueOf(value.trim().toUpperCase());
+	}
 }
--- user-service/src/main/java/com/princesses7/findy/user/user/entity/Role.java
+++ user-service/src/main/java/com/princesses7/findy/user/user/entity/Role.java
@@ -1,5 +1,12 @@
 package com.princesses7.findy.user.user.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Role {
-	ROLE_USER, ROLE_ADMIN
+	ROLE_USER, ROLE_ADMIN;
+
+	@JsonCreator
+	public static Role from(String value) {
+		return Role.valueOf(value.trim().toUpperCase());
+	}
 }
