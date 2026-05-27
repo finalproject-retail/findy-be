092d0edc75716c52c01a6d49f83d251d6ca57668
gyuoo <yeomky98@naver.com>
2026-05-27 23:22:41 +0900

    chore: analytics-service 프로젝트 생성

---
+++ analytics-service/src/test/java/com/princesses7/findy/analytics/AnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.princesses7.findy.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
