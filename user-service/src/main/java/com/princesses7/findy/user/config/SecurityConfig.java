79c70347ac15f61b1e08c50d2155d379666052e2
chaelyn-kim <chaelyn.kim1125@gmail.com>
2026-05-28 14:08:02 +0900

    feat: 서비스 securityconfig 수정

--- user-service/src/main/java/com/princesses7/findy/user/config/SecurityConfig.java
+++ user-service/src/main/java/com/princesses7/findy/user/config/SecurityConfig.java
@@ -22,14 +22,16 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf(csrf -> csrf.disable()) // 로컬/MSA 환경에선 보통 끔
-			.sessionManagement(
-				session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 대신 JWT 사용
-			.authorizeHttpRequests(auth -> auth
-				.requestMatchers("/api/v1/auth/login", "/api/v1/users/signup").permitAll()
-				.anyRequest().authenticated() // 나머지는 토큰 없으면 차단!
-			)
-			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+				.csrf(csrf -> csrf.disable()) // 로컬/MSA 환경에선 보통 끔
+				.sessionManagement(
+						session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 대신 JWT 사용
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/api/v1/auth/login", "/api/v1/users/signup", "/actuator/health",
+								"/actuator/prometheus")
+						.permitAll()
+						.anyRequest().authenticated() // 나머지는 토큰 없으면 차단!
+				)
+				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
 	}
