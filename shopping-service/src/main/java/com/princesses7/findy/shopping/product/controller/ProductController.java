e841aaa212bdd257a3b234c0c3a1bba44abf9b27
gyuoo <yeomky98@naver.com>
2026-05-28 15:27:24 +0900

    comment: 주석에 TODO 추가

--- shopping-service/src/main/java/com/princesses7/findy/shopping/product/controller/ProductController.java
+++ shopping-service/src/main/java/com/princesses7/findy/shopping/product/controller/ProductController.java
@@ -26,7 +26,7 @@ public class ProductController {
 		@RequestParam(required = false) String keyword,
 		@RequestParam(defaultValue = "0") int page,
 		@RequestParam(defaultValue = "20") int size,
-		// 인기순 정렬은 Redis 랭킹 데이터 연동 시 별도 구현 예정
+		// TODO: 인기순 정렬은 Redis 랭킹 데이터 연동 시 별도 구현 예정
 		@RequestParam(defaultValue = "createdAt") String sortBy,
 		@RequestParam(defaultValue = "desc") String direction
 	) {
