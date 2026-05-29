a725e5b6fd3b0610fcdc6b5b021651cab80cdb48
gyuoo <yeomky98@naver.com>
2026-05-29 10:41:05 +0900

    refactor: HACCP 상품 조회 API 응답 형식 통일

--- shopping-service/src/main/java/com/princesses7/findy/shopping/external/haccp/HaccpProductController.java
+++ shopping-service/src/main/java/com/princesses7/findy/shopping/external/haccp/HaccpProductController.java
@@ -3,24 +3,28 @@ package com.princesses7.findy.shopping.external.haccp;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.princesses7.findy.shopping.external.haccp.dto.response.HaccpProductItemResponse;
+import com.princesses7.findy.shopping.global.response.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/haccp/products")
 public class HaccpProductController {
 
 	private final HaccpProductClient haccpProductClient;
 
-	// TODO: ApiResponse 사용?
-	@GetMapping("/api/v1/haccp/products")
-	public List<HaccpProductItemResponse> search(
+	@GetMapping
+	public ApiResponse<List<HaccpProductItemResponse>> search(
 		@RequestParam String productName
 	) {
-		return haccpProductClient.searchByProductName(productName);
+		List<HaccpProductItemResponse> response = haccpProductClient.searchByProductName(productName);
+
+		return ApiResponse.ok("HACCP 제품이미지 및 포장지표기정보 조회에 성공했습니다.", response);
 	}
 }
