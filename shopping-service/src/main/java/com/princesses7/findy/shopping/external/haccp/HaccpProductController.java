94592e36b5c34676cfcc0a7a6e37bf725c3f33e5
gyuoo <yeomky98@naver.com>
2026-05-29 09:54:58 +0900

    feat: HACCP API 관련 controller 추가

---
+++ shopping-service/src/main/java/com/princesses7/findy/shopping/external/haccp/HaccpProductController.java
@@ -0,0 +1,25 @@
+package com.princesses7.findy.shopping.external.haccp;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.princesses7.findy.shopping.external.haccp.dto.response.HaccpProductItemResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class HaccpProductController {
+
+	private final HaccpProductClient haccpProductClient;
+
+	@GetMapping("/api/v1/haccp/products")
+	public List<HaccpProductItemResponse> search(
+		@RequestParam String productName
+	) {
+		return haccpProductClient.searchByProductName(productName);
+	}
+}
