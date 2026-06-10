4d5db630501c2ce0c2d113693edcc2246a53da4d
gyuoo <yeomky98@naver.com>
2026-06-10 14:04:27 +0900

    feat: 챗봇 일반 상품 추천 의도 추가

--- recommendation-service/src/main/java/com/princesses7/findy/recommendation/chatbot/entity/ChatIntent.java
+++ recommendation-service/src/main/java/com/princesses7/findy/recommendation/chatbot/entity/ChatIntent.java
@@ -3,6 +3,7 @@ package com.princesses7.findy.recommendation.chatbot.entity;
 public enum ChatIntent {
 	GENERAL,
 	RECIPE_INGREDIENT_RECOMMENDATION,
+	GENERAL_PRODUCT_RECOMMENDATION,
 	PRODUCT_SEARCH,
 	INVENTORY_INQUIRY,
 	PROMOTION_INQUIRY,
