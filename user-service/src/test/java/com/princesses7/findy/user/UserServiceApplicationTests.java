50b8980fa646bb44769035d8d7c6424d9a5ea658
gyuoo <yeomky98@naver.com>
2026-06-11 14:34:58 +0900

    fix: 비밀번호 재설정 메일 발송 Bean 설정 추가

--- user-service/src/test/java/com/princesses7/findy/user/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/princesses7/findy/user/UserServiceApplicationTests.java
@@ -3,12 +3,7 @@ package com.princesses7.findy.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(
-	properties = {
-		"spring.autoconfigure.exclude="
-			+ "org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration"
-	}
-)
+@SpringBootTest
 class UserServiceApplicationTests {
 
 	@Test
