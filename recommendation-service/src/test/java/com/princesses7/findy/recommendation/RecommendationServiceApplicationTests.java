4c12a891564d332f2b08ccdf537ac6501028abc3
gyuoo <yeomky98@naver.com>
2026-06-16 16:44:03 +0900

    test: 추천 서비스 컨텍스트 테스트 스키마 검증 비활성화

--- recommendation-service/src/test/java/com/princesses7/findy/recommendation/RecommendationServiceApplicationTests.java
+++ recommendation-service/src/test/java/com/princesses7/findy/recommendation/RecommendationServiceApplicationTests.java
@@ -3,7 +3,9 @@ package com.princesses7.findy.recommendation;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+	"spring.jpa.hibernate.ddl-auto=none"
+})
 class RecommendationServiceApplicationTests {
 
 	@Test
